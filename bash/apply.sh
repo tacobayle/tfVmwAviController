783a0e1b9e2e878108816cc2b3aa2b1bddd04a52
tacobayle <nicolas.bayle@gmail.com>
2022-06-10 11:00:21 +0000

    20220610_cleanup

--- bash/apply.sh
+++ bash/apply.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd tfVmwAviController/01_infra
-terraform init
-terraform apply -auto-approve
+cd 01_infra ; terraform init ; terraform apply -auto-approve
 cd ../02_avi_username; terraform init; terraform apply -auto-approve -no-color -var-file=../controllers.json
 cd ../03_avi_config; terraform init; terraform apply -auto-approve -no-color -var-file=../controllers.json -var-file=../.password.json
 cd ../04_avi_cluster; terraform init; terraform apply -auto-approve -no-color -var-file=../controllers.json  -var-file=../.password.json
-cd ../05_avi_vcenter_cc; terraform init; terraform apply -auto-approve -no-color -var-file=../controllers.json  -var-file=../.password.json
+# cd ../05_avi_vcenter_cc; terraform init; terraform apply -auto-approve -no-color -var-file=../controllers.json  -var-file=../.password.json
