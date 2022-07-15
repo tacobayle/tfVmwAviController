b33461b2167ce15cc9f447f662154899b46b399c
tacobayle <nicolas.bayle@gmail.com>
2022-07-15 11:12:23 +0000

    20220715_check_for_downloaded_file

--- 01_infra/vsphere_content_library.tf
+++ 01_infra/vsphere_content_library.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "download_avi" {
   provisioner "local-exec" {
-    command = "if [ -f $(basename \"${var.avi_controller_url}\" | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//') ]; then echo \"$(basename \"${var.avi_controller_url}\" | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//') exists.\" ; else curl --silent -o $(basename \"${var.avi_controller_url}\" | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//') \"${var.avi_controller_url}\" ; fi"
+    command = "if [ -f $(basename '${var.avi_controller_url}' | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//') ]; then echo \"$(basename '${var.avi_controller_url}' | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//') exists.\" ; else curl --silent -o $(basename '${var.avi_controller_url}' | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\/tmp\//') '${var.avi_controller_url}' ; fi"
   }
 }
 
@@ -20,6 +20,6 @@ resource "null_resource" "remove_download_avi" {
   depends_on = [vsphere_content_library_item.file]
   count = (var.controller.remove_file == true ? 1 : 0)
   provisioner "local-exec" {
-    command = "rm -f $(basename \"${var.avi_controller_url}\" | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//')"
+    command = "rm -f $(basename '${var.avi_controller_url}' | sed -e 's/.ova.*/.ova/g') | sed -e 's/^/\\/tmp\\//')"
   }
 }
