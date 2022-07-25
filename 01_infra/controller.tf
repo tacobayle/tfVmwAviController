5a625a6ca08bd2a6b5b54faa048d0bb49fa42b1b
tacobayle <nicolas.bayle@gmail.com>
2022-07-25 09:08:49 +0000

    20220725_22.1.1

--- 01_infra/controller.tf
+++ 01_infra/controller.tf
@@ -162,18 +162,18 @@ resource "local_file" "output_json_file_dhcp_cluster" {
 
 resource "local_file" "output_json_file_dhcp_standalone" {
   count            = (var.dhcp == true && var.avi_cluster== false ? 1 : 0)
-  content     = "{\"avi_controller_ips\": ${jsonencode(vsphere_virtual_machine.controller_dhcp_standalone.*.default_ip_address)}}, \"content_library_id\": \"${vsphere_content_library.library.id}\"}"
+  content     = "{\"avi_controller_ips\": ${jsonencode(vsphere_virtual_machine.controller_dhcp_standalone.*.default_ip_address)}, \"content_library_id\": \"${vsphere_content_library.library.id}\"}"
   filename = "../controllers.json"
 }
 
 resource "local_file" "output_json_file_static_cluster" {
   count            = (var.dhcp == false && var.avi_cluster== true ? 1 : 0)
-  content     = "{\"avi_controller_ips\": ${jsonencode(vsphere_virtual_machine.controller_static_cluster.*.default_ip_address)}}, \"content_library_id\": \"${vsphere_content_library.library.id}\"}"
+  content     = "{\"avi_controller_ips\": ${jsonencode(vsphere_virtual_machine.controller_static_cluster.*.default_ip_address)}, \"content_library_id\": \"${vsphere_content_library.library.id}\"}"
   filename = "../controllers.json"
 }
 
 resource "local_file" "output_json_file_static_standalone" {
   count            = (var.dhcp == false && var.avi_cluster== false ? 1 : 0)
-  content     = "{\"avi_controller_ips\": ${jsonencode(vsphere_virtual_machine.controller_static_standalone.*.default_ip_address)}}, \"content_library_id\": \"${vsphere_content_library.library.id}\"}"
+  content     = "{\"avi_controller_ips\": ${jsonencode(vsphere_virtual_machine.controller_static_standalone.*.default_ip_address)}, \"content_library_id\": \"${vsphere_content_library.library.id}\"}"
   filename = "../controllers.json"
 }
