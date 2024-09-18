ba0f0004e7f1687efc0e9a755175b629bf6167eb
tacobayle <nicolas.nayle@gmail.com>
2024-09-18 09:28:58 +0200

    20240913_destroy

--- bash/destroy.sh
+++ bash/destroy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 cd 07_waf_policy
 rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
-cd ../06_avi_vs_no_ssl
+cd ../07_avi_vs_no_ssl
 rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
-cd ../06_avi_vs
+cd ../07_avi_vs_bgp
+rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
+cd ../07_avi_vs
+rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
+cd ../06_vrf_bgp
+rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
+cd ../06_vrf_bgp
 rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
 cd ../05_avi_vcenter_cc
 rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
@@ -14,5 +20,6 @@ rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
 cd ../02_avi_username
 rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
 cd ../01_infra
+terraform init
 terraform destroy -auto-approve -no-color
 rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
